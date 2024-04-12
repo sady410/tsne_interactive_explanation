2bf78c96a341ff1f87f7dbb6c8745fbfc65f48cb
Thibaut Septon <tsepton@gmail.com>
2024-04-12 14:49:23 +0200

    Update layout

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -12,102 +12,59 @@ from plots import (create_average_feature_distribution_plot,
 
 
 def overview_card():
-    return html.Div(
-        [
-            html.Div(
-                [
-                    dcc.Loading(
-                        id="loading-overview-plot",
-                        type="circle",
-                        className="overview-plot ",
-                        children=[
-                            dcc.Graph(
-                                id='overview-plot', config={'displayModeBar': False, 'staticPlot': True}, className="overview-plot ")
-                        ]
-                    )
-
-                ]
-            )
-        ], className=""
-    )
+    return dcc.Graph(
+        id='overview-plot', config={'displayModeBar': False, 'staticPlot': True}, className="overview-plot ")
 
 
 def scatter_plot_card():
-    return html.Div(
-        [
-            html.Div(
-                [
-                    dcc.Loading(
-                        id="loading-tsne-plot",
-                        type="circle",
-                        className="main-plot",
-                        children=[
-                            dcc.Graph(
-                                id='tsne-plot',  config={'displaylogo': False, 'displayModeBar': True, 'modeBarButtonsToRemove': ['zoomIn2d', 'zoomOut2d', 'toImage', 'autoScale2d', 'select2d']}, className="main-plot")
-                        ]
-                    )
-                ],
-            )
-        ], className="",
-    )
+    return dcc.Graph(
+        id='tsne-plot',
+        config={'displaylogo': False,
+                'displayModeBar': True,
+                'modeBarButtonsToRemove': ['zoomIn2d', 'zoomOut2d', 'toImage', 'autoScale2d', 'select2d']},
+        className="main-plot")
 
 
 def explanation_plot_card():
     return html.Div(
         [
-            html.Div("Explanation Plot", className="section-title"), # TODO: Change title explanation is global
-            html.Div(
-                [
-                    dcc.Graph(id='explanation-barplot', 
-                              config={'displayModeBar': False}, className="explanation-plot")
-                ]
-            )
-        ],
-        className="",
+            dcc.Graph(id='explanation-barplot',
+                      config={'displayModeBar': False}, className="explanation-plot")
+        ], className=""
     )
 
 
 def feature_distribution_plot():
-    return html.Div([
-        dbc.Col([
-            html.Div([
-                html.Div("Feature distribution", className="section-title"),
-                html.Div(
-                [
-                    dcc.Graph(id='feature-distribution-plot',
-                              config={'displayModeBar': False}, className="feature-distribution-plot")
-                ]
-            )
-            ])
-        ])
-    ])
+    return dcc.Graph(id='feature-distribution-plot',
+                      config={'displayModeBar': False}, className="feature-distribution-plot")
+   
+    
 
 
 def layout():
     return html.Div([
         html.Div([
-            html.Div([
-                html.Div([
-                    overview_card()
-                ], className="d-flex flex-column  justify-content-between"),
-                html.Div([
-                    scatter_plot_card()
-                ], className="flex-grow-1 max-content"),
-                html.Div([
-                    explanation_plot_card()
-                ], className=""),
-            ], className="d-flex justify-content-center"),
-
-            html.Div([
-
-                html.Div([
-                    feature_distribution_plot()
-                ], className=""),
-
-            ], className="d-flex justify-content-center "),  # TODO
-
-        ], className="flex-grow-1 max-width-container ")
-    ], className="d-flex justify-content-center")
+                    html.Div([
+                        html.Div("Overview Plot",
+                                 className="section-title"),
+                        overview_card(),
+                    ]),
+                    html.Div([
+                        html.Div("Feature distribution",
+                                 className="section-title"),
+                        feature_distribution_plot()],
+                        className="section-title")
+                    ], className="sub-container-1"),
+        html.Div([
+            html.Div("t-SNE",
+                     className="section-title"),
+            scatter_plot_card()
+        ], className="sub-container-2"),
+        html.Div([
+            html.Div("Explanation Plot", className="section-title"),
+            explanation_plot_card()
+        ], className="sub-container-3"),
+    ], className="main-container")
 
 
 @dash.callback(
@@ -136,7 +93,7 @@ def update_overview_plot(tsne_data, relayout_data):
         figure_json = tsne_data.get('tsne_scatterplot')
         if figure_json:
             fig = json.loads(figure_json)
-    
+
     if relayout_data is not None and 'xaxis.range[0]' in relayout_data:
         x0 = relayout_data['xaxis.range[0]']
         x1 = relayout_data['xaxis.range[1]']
@@ -150,7 +107,7 @@ def update_overview_plot(tsne_data, relayout_data):
         # Retrieve existing figure layout or create a new layout if it doesn't exist
         layout = fig['layout']
         shapes = layout.get('shapes', [])
-        
+
         # Add rectangle shape
         shapes.append({
             'type': 'rect',
@@ -163,7 +120,7 @@ def update_overview_plot(tsne_data, relayout_data):
                 'width': 3
             }
         })
-        
+
         # Update layout with new shapes
         layout['shapes'] = shapes
         fig['layout'] = layout
@@ -174,8 +131,8 @@ def update_overview_plot(tsne_data, relayout_data):
 @dash.callback(
     Output('explanation-barplot', 'figure'),
     [Input('tsne-data', 'data'),
-    Input('tsne-plot', 'selectedData'),
-    Input('tsne-plot', 'clickData')]
+     Input('tsne-plot', 'selectedData'),
+     Input('tsne-plot', 'clickData')]
 )
 def update_explanation_bar_plot(tsne_data, selected_data, click_data):
     ctx = dash.callback_context
@@ -189,20 +146,22 @@ def update_explanation_bar_plot(tsne_data, selected_data, click_data):
 
         if triggered_component_id == 'tsne-plot':
             if selected_data is not None and selected_data['points'] != []:
-                selected_indices = [point['customdata'][0] for point in selected_data['points']]
-                return create_combined_gradients_plot(gradients, feature_names, selected_indices[0]) 
+                selected_indices = [point['customdata'][0]
+                                    for point in selected_data['points']]
+                return create_combined_gradients_plot(gradients, feature_names, selected_indices[0])
             elif click_data is not None:
                 selected_indices = [click_data['points'][0]['customdata'][0]]
-                return create_combined_gradients_plot(gradients, feature_names, selected_indices[0]) 
+                return create_combined_gradients_plot(gradients, feature_names, selected_indices[0])
 
-        return create_feature_importance_ranking_plot(gradients, feature_names)   
+        return create_feature_importance_ranking_plot(gradients, feature_names)
     return {}
+
 
 @dash.callback(
     Output('feature-distribution-plot', 'figure'),
     [Input('tsne-data', 'data'),
-    Input('tsne-plot', 'selectedData'),
-    Input('tsne-plot', 'clickData')]
+     Input('tsne-plot', 'selectedData'),
+     Input('tsne-plot', 'clickData')]
 )
 def update_feature_distribution_plot(tsne_data, selected_data, click_data):
     ctx = dash.callback_context
@@ -218,12 +177,14 @@ def update_feature_distribution_plot(tsne_data, selected_data, click_data):
 
         if triggered_component_id == 'tsne-plot':
             if selected_data is not None and selected_data['points'] != []:
-                selected_indices = [point['customdata'][0] for point in selected_data['points']]
+                selected_indices = [point['customdata'][0]
+                                    for point in selected_data['points']]
             elif click_data is not None:
-                selected_indices = [click_data['points'][0]['customdata'][0]]    
-        
+                selected_indices = [click_data['points'][0]['customdata'][0]]
+
         return create_average_feature_distribution_plot(feature_names, X, selected_indices)
 
     return {}
+
 
 dash.register_page(__name__)
